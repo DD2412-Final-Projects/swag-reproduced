0535006c9633d041fb2e1e6bfd5b7010d19c1783
Josef Malmström <josef.malmstrom@gmail.com>
2019-10-12 14:19:45 +0200

    created basic file structure and added vgg16 implementation

---
+++ test.py
@@ -0,0 +1,9 @@
+"""
+Reads learned weight distribution parameters from file,
+and tests using the SWAG test procedure.
+
+1. Load the learned parameters.
+2. Load test data.
+3. Run SWAG test procedure for each sample.
+4. Compute metrics and generate plots (e.g. NLL, reliability diagrams).
+"""
---
+++ train.py
@@ -0,0 +1,8 @@
+"""
+Trains a model using SWAG and saves the learned weight distribution parameters.
+
+1. Load/import the network architecture from the 'networks' directory.
+2. Load training data.
+3. Train the model with SWAG.
+4. Save the learned weight distribution parameters.
+"""
